a50b85cb39b01f7e2f6c4947453977c75458f809
Javibu13 <javiersanzmoline@gmail.com>
2025-04-04 00:59:47 +0200

    Add logger and methods to launch image selection from filesystem

--- src/main/java/com/sanvalero/imagefilters/App.java
+++ src/main/java/com/sanvalero/imagefilters/App.java
@@ -1,5 +1,8 @@
 package com.sanvalero.imagefilters;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -12,15 +15,18 @@ import java.io.IOException;
  * JavaFX App
  */
 public class App extends Application {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
 
     private static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
+        logger.info("Starting JavaFX application...");
         scene = new Scene(loadFXML("main"), 900, 600);
         stage.setScene(scene);
         stage.setTitle("Image Filters");
         stage.show();
+        logger.info("JavaFX application started successfully.");
     }
 
     static void setRoot(String fxml) throws IOException {
